50d124e27390d6bf9169af16a9bfbc1418a4924c
Guilherme de Morais Teixeira <guimteixeira@yahoo.com.br>
2013-02-07 22:08:44 -0200

    Primeiro upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Chamados -  SEGG HelpDesk</title>
+<link rel="stylesheet" type="text/css" href="estilos/gerais.css"/>
+</head>
+
+<body><img src="imagens/logo.png" name="logo" width="254" height="120" id="logo" />
+<h1> Sistema HelpDesk <img src="imagens/help.png" width="62" height="48" /></h1>
+<div id="informacoesGerais">Olá, <strong>Fulano</strong>. | <a href="logout.php"><img src="imagens/sair.png" alt="Sair do Sistema" width="16" height="16" /></a></div>
+<p id="localizacao">Listagem de chamados</p>
+<p>&nbsp;</p>
+<table width="200" border="1" id="chamados">
+  <tr>
+    <th>&nbsp;</th>
+    <th>ID Chamado</th>
+    <th>Data/Hora Criação</th>
+    <th>Situação</th>
+    <th>Sistema</th>
+    <th>Tipo</th>
+    <th>Título</th>
+    <th>Anexos</th>
+    <th>&nbsp;</th>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>#</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>Encerrar</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>#</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>Encerrar</td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<p class="centralizar">Sistema desenvolvido por <strong>SEGG Softwares</strong></p>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Login -  SEGG HelpDesk</title>
+<link rel="stylesheet" type="text/css" href="estilos/gerais.css"/>
+</head>
+
+<body><img src="imagens/logo.png" name="logo" width="254" height="120" id="logo" />
+<h1> Sistema HelpDesk <img src="imagens/help.png" width="62" height="48" /></h1>
+<div id="informacoesGerais">Sistema de Gerenciamento de Chamados e Suporte</div>
+<div id="login">
+  <p>&nbsp;</p>
+  <h2><img src="imagens/login.png" width="24" height="24" />&nbsp;ACESSO AO SISTEMA</h2>
+  <p>&nbsp;</p>
+  <p>Seja bem-vindo ao <strong>SEGG HelpDesk</strong>.</p>
+  <p>Efetue seu login para ter acesso ao sistema.</p>
+  <p>&nbsp;</p>
+  <form id="formLogin" name="formLogin" method="post" action="">
+    <p>
+      <label for="usuario"><br />
+      </label>
+      <input name="usuario" type="text" id="usuario" />
+    </p>
+    <p>&nbsp;</p>
+    <p>
+      <label for="senha"></label>
+      <input name="senha" type="password" id="senha" />
+    </p>
+    <p>&nbsp;</p>
+    <p><a href="#" class="okButton">ACESSAR</a></p>
+  </form>
+  <p>&nbsp;</p>
+</div>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p class="centralizar">Sistema desenvolvido por <strong>SEGG Softwares</strong></p>
+</body>
+</html>
